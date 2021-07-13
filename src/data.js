b48c4a0b02b60de06648961badb92b2a845aecdd
DianaAraujoG <dianac251@gmail.com>
2021-07-13 12:30:41 -0500

    HTML Y posters

--- src/data.js
+++ src/data.js
@@ -1,7 +1,13 @@
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const posters = (data) => {
+    let DirPoster = [20];
+    let i=0;
+    do {
+      DirPoster[i]= data.films[i].poster;
+      i++
+      console.log(DirPoster[i]);
+    } while (i<20);
+    return DirPoster;
 };
 
 export const anotherExample = () => {
