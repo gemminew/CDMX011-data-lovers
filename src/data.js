8400f4c4be818b52e15cb1b4394a7f357e653754
DianaAraujoG <dianac251@gmail.com>
2021-07-13 15:30:00 -0500

    Creo que lo arregle

--- src/data.js
+++ src/data.js
@@ -1,13 +1,7 @@
 // estas funciones son de ejemplo
-export const posters = (data) => {
-    let DirPoster = [20];
-    let i=0;
-    do {
-      DirPoster[i]= data.films[i].poster;
-      i++
-      console.log(DirPoster[i]);
-    } while (i<20);
-    return DirPoster;
+
+export const example = () => {
+  return 'example';
 };
 
 export const anotherExample = () => {
