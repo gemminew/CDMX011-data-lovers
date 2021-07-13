5964d95798463019f90626fef8a9e56936420ba0
Gemma <gemminew@gmail.com>
2021-07-13 17:20:23 -0500

    simplificar iteraciones

--- src/main.js
+++ src/main.js
@@ -6,26 +6,11 @@ import data from './data/ghibli/ghibli.js';
 let Adress = [20];
 Adress = posters(data);
 
-document.getElementById("film0").src=Adress[0];
-document.getElementById("film1").src=Adress[1];
-document.getElementById("film2").src=Adress[2];
-document.getElementById("film3").src=Adress[3];
-document.getElementById("film4").src=Adress[4];
-document.getElementById("film5").src=Adress[5];
-document.getElementById("film6").src=Adress[6];
-document.getElementById("film7").src=Adress[7];
-document.getElementById("film8").src=Adress[8];
-document.getElementById("film9").src=Adress[9];
-document.getElementById("film11").src=Adress[11];
-document.getElementById("film12").src=Adress[12];
-document.getElementById("film13").src=Adress[13];
-document.getElementById("film14").src=Adress[14];
-document.getElementById("film15").src=Adress[15];
-document.getElementById("film16").src=Adress[16];
-document.getElementById("film17").src=Adress[17];
-document.getElementById("film18").src=Adress[18];
-document.getElementById("film19").src=Adress[19];
 
+
+for(i = 0; i <20; i++){ 
+    document.getElementById("film" + i).src=Adress[i];
+}
 /*console.log(posters, data);
 console.log(data.films[2].title);
 console.log(data.films[0].people[1].name);
