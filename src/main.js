4ca79ebca6c9ffddbe868b554d82a4530f1c59f4
DianaAraujoG <dianac251@gmail.com>
2021-07-16 14:01:23 -0500

    Commit ya no se ni que hice aqui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { posters, filteredOut} from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+console.log(data)
+
 let Adress = [20];
 Adress = posters(data);
 
