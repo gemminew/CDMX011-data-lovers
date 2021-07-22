bf0921a677dafec16adf225170e7abf5bb14d549
DianaAraujoG <dianac251@gmail.com>
2021-07-21 22:48:21 -0500

    Order by

--- src/data.js
+++ src/data.js
@@ -16,6 +16,47 @@ export const getUniqueValues = (array) => (
       arr.indexOf(currentValue) === index
   ))
 )
+//Parte del order alfabeticamente, year and score
+export const orderBy = (films, option) =>{
+  
+  if( option == 'AZ'){
+    const orderByAtoZ = films.sort((film1, film2)=>{
+      return (film1.title < film2.title) ? -1 : 1;
+    })
+    console.log(orderByAtoZ)
+  }
+  else if (option == 'ZA'){
+    const orderByZtoA = films.sort((film1, film2)=>{
+      return (film1.title > film2.title) ? -1 : 1;
+    })
+    console.log(orderByZtoA)
+  }
+  else if (option == 'scoreMinToMax'){
+    const orderByMinToMax = films.sort((film1, film2)=>{
+      return film1.rt_score - film2.rt_score
+    })
+    console.log(orderByMinToMax)
+  }
+  else if (option== 'scoreMaxToMin'){
+    const orderByMaxToMin = films.sort((film1, film2)=>{
+      return film2.rt_score - film1.rt_score
+    })
+    console.log(orderByMaxToMin)
+  }
+  else if (option == 'dateMinToMax'){
+    const dateByMinToMax = films.sort((film1, film2) =>{
+      return film1.release_date - film2.release_date
+    })
+    console.log(dateByMinToMax)
+  }
+  else if (option == 'dateMaxToMin'){
+    const dateByMaxToMin = films.sort((film1, film2) =>{
+      return film2.release_date - film1.release_date
+    })
+    console.log(dateByMaxToMin )
+  }
+}
+
 
 //obtener valores unicos en select input
 //
