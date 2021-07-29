7dc3b29e78d32874a6576e9fbdf164ab3c8eee68
Gemma <gemminew@gmail.com>
2021-07-29 13:13:42 -0500

    test

--- test/data.spec.js
+++ test/data.spec.js
@@ -755,13 +755,6 @@ describe('directorFilter', () => {
   });
 });
 
-describe('posters', () => {
-  
-  it('deberia retornar el arreglo de posters', () => {
-    expect(typeof posters).toBe('function')
-   // expect(Array.isArray(result)).toBe(true)
-  })
-});
 
 describe('characterFilter', () => {
   it('is a function', () => {
