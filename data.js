61550be1d6f653891aac0980e2c9fa1c95222f4e
Gemma <gemminew@gmail.com>
2021-07-29 22:56:44 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,144 @@
+export const posters = (data) => {
+    let DirPoster = [];
+    let i=0;
+    do {
+      DirPoster.push(data.films[i].poster);
+      //console.log(DirPoster[i]);
+      i++
+
+    } while (i<data.films.length);
+      //console.log(DirPoster[i]);
+    return DirPoster;
+};
+
+export const getUniqueValues = (array) => ( 
+  array.filter((currentValue, index, arr) => (
+      arr.indexOf(currentValue) === index
+  )) 
+)
+
+//-----------------------Parte del order alfabeticamente, year and score
+export const orderBy = (films, option) =>{
+  if( option == 'AZ'){
+    const orderByAtoZ = films.sort((film1, film2)=>{
+      return (film1.title < film2.title) ? -1 : 1;
+    })
+    console.log(orderByAtoZ)
+    return orderByAtoZ //Agrege esto porque no regresaba nada
+  }
+  else if (option == 'ZA'){
+    const orderByZtoA = films.sort((film1, film2)=>{
+      return (film1.title > film2.title) ? -1 : 1;
+    })
+    return orderByZtoA
+  }
+  else if (option == 'scoreMinToMax'){
+    const orderByMinToMax = films.sort((film1, film2)=>{
+      return film1.rt_score - film2.rt_score
+    })
+    return orderByMinToMax
+  }
+  else if (option== 'scoreMaxToMin'){
+    const orderByMaxToMin = films.sort((film1, film2)=>{
+      return film2.rt_score - film1.rt_score
+    })
+    return orderByMaxToMin
+  }
+  else if (option == 'dateMinToMax'){
+    const dateByMinToMax = films.sort((film1, film2) =>{
+      return film1.release_date - film2.release_date
+    })
+    return dateByMinToMax 
+  }
+  else if (option == 'dateMaxToMin'){
+    const dateByMaxToMin = films.sort((film1, film2) =>{
+      return film2.release_date - film1.release_date
+    })
+    return dateByMaxToMin
+  }
+}
+
+
+//obtener valores unicos en select input
+//
+export const directorFilter = (dataInput,films) => {
+    const directorFilms = films.filter(function(films){
+       return films.director === dataInput;})
+       .map(function(films){
+         return films 
+       })
+       console.log(directorFilms);
+       
+       return directorFilms
+}
+
+export const joinPeopleName = (film) => {
+  let numberPeople = film.people;
+  let namesOfPeople = [];
+    for(let j=0; j<numberPeople.length; j++){
+      namesOfPeople[j] = film.people[j].name ;
+    }
+    let names = namesOfPeople.join(', ');
+
+    console.log(names)
+ return names 
+}
+
+export const producerFilter = (dataInput,films) => {
+
+  const producers = films.filter(function(films){
+     return films.producer === dataInput;})
+     .map(function(films){
+      return films
+     })
+     return producers
+}
+
+export const characterFilter = (dataInput,films) => {
+
+  const characters = films // parameter
+    .filter( function(banana){ return banana.title === dataInput;} ) 
+    .map(function(banana){
+      let listOfPeople = banana.people;
+      console.log('characters:')
+      console.log(listOfPeople.length)
+      return listOfPeople
+    })[0] // 
+    return characters
+  }
+
+
+export const titleLocation = (films) =>{
+  let titles = ['']
+  let j=0
+  for(let i=0; i< films.length ;i++){
+    let location = films[i].locations
+    if( location.length != 0){
+      titles[j]= films[i].title
+      j++
+    }else{continue}
+  }
+  return titles
+}
+
+export const locationFilter = (dataInput,films) =>{
+  const locations = films.filter(function(film){return film.title === dataInput})
+  .map(function(film){
+    return film.locations
+  })[0]
+  return locations
+}
+
+  export const vehicleFilter = (dataInput, films) => {
+
+    const vehicles = films 
+    .filter(function(film){ return film.title == dataInput;})
+    .map(function(film){
+      let listOfVehicles = film.vehicles;
+      return listOfVehicles
+    }) [0]
+    return vehicles
+  }
+  
+
+
---
+++ main.js
@@ -0,0 +1,356 @@
+
+import { posters, orderBy, directorFilter, producerFilter, characterFilter, getUniqueValues, titleLocation,locationFilter, vehicleFilter,joinPeopleName} from './data.js';
+import data from '../data/ghibli/ghibli.js';
+
+console.log(data)
+let Adress = posters(data);
+///---------- Seccion de la primera pagina donde muestra los posters
+let posterHTML = ""
+Adress.forEach(objeto =>{
+    posterHTML+=generatorPosterHTML(objeto)
+})
+function generatorPosterHTML(objeto){
+    return `<div class="poster"><img src="${objeto}" ></div>`
+}
+
+document.getElementById("movies").innerHTML = posterHTML
+//document.getElementById("pruebaPeople").src = data.films[5].people[0].img
+let sortAndSelect = document.getElementById('sortinGhibli')
+sortAndSelect.addEventListener('change', () =>{
+    let posterOrder = orderBy(data.films, sortAndSelect.value)
+    let posterOrderHTML = '' //Aqui se guarda el nuevo orden
+    console.log(posterOrder)
+    posterOrder.forEach(objeto =>{
+        posterOrderHTML+=generatorPosterHTML(objeto)
+    })
+    // if(sortAndSelect.value == 'AZ' || sortAndSelect.value == 'ZA'){ //NO ME DEJA METER LA FUNCION EN EL IF  :(
+    function generatorPosterHTML(objeto){
+        if(sortAndSelect.value == 'AZ' || sortAndSelect.value == 'ZA'){
+        return `<figure class="poster">
+        <figcaption class='princialPoster'> ${objeto.title}</figcaption>
+        <img src="${objeto.poster}" >
+        </figure>`
+        }else
+        if (sortAndSelect.value == 'scoreMinToMax' || sortAndSelect.value == 'scoreMaxToMin'){
+            return `<figure class="poster">
+            <figcaption class='princialPoster'>Score: ${objeto.rt_score}</figcaption>
+            <img src="${objeto.poster}" >
+            </figure>`
+        }else
+        if (sortAndSelect.value == 'dateMinToMax' || sortAndSelect.value == 'dateMaxToMin'){
+            return `<figure class="poster">
+            <figcaption class='princialPoster'>Date: ${objeto.release_date}</figcaption>
+            <img src="${objeto.poster}" >
+            </figure>`
+        }
+    }
+    document.getElementById("movies").innerHTML = posterOrderHTML
+})
+
+
+//SELECCIONAR DIRECTOR directo DESDE la DATA // ----------------seccion de DIRECTORES
+let selectDir = document.getElementById('selectDirector');
+let directorOptions = data.films.map(function(film) {return film.director})
+directorOptions = getUniqueValues(directorOptions) //evita duplicacion de elementos
+    console.log(directorOptions);
+    for (let i = 0; i < directorOptions.length; i++){
+        let directorName = directorOptions[i];
+        let dirElement = document.createElement("option")
+        
+        dirElement.textContent = directorName 
+        dirElement.value = directorName
+        selectDir.appendChild(dirElement);
+    }
+    selectDir.addEventListener('change', () => {
+     document.getElementById('FirstPage').style.display="none";
+     document.getElementById('informativeFile').style.display = 'block';
+     document.getElementById('character').style.display = 'none';
+     document.getElementById('locations').style.display = 'none';
+     document.getElementById('vehicles').style.display = 'none';
+     document.getElementById('vehiclesFile').style.display = 'none'
+     
+     let dataInput = selectDir.value;
+     let dataFiltered =['']
+     dataFiltered = directorFilter(dataInput,data.films);  
+     const caption = 'He has directed '+ dataFiltered.length + ' films';
+    
+     console.log(directorFilter("Director No definido",data.films))
+
+     document.getElementById('Name').innerHTML = dataInput;
+     document.getElementById('caption').innerHTML = caption;
+     console.log('Numero de peliculas: '+ dataFiltered.length)
+    console.log(dataFiltered)
+     /* ----- HTML DINAMICO*/
+     let directorHTML = ""
+     dataFiltered.forEach( oneDirector =>{
+         oneDirector.names = joinPeopleName(oneDirector)
+         directorHTML += generatorDirectorHTML(oneDirector)
+     })
+     function generatorDirectorHTML(oneDirector){
+         return ` 
+         <div class="flexRow" style="display: flex; font-family: sans-serif; flex-flow: nowrap; 
+         background-color: #D9B8B5; border-radius: 1rem; margin-bottom: 1rem;">
+            <div class="posterFilter" style="padding: 1rem;">
+                <img src="${oneDirector.poster}" id="filterImg"></div>
+            <div class="Informative">
+            <h1>${oneDirector.title}</h1><hr color="#000">
+            <p><b>Year: </b><span id="year">${oneDirector.release_date}</span></p>
+            <p><b>Score: </b><span id="score">${oneDirector.rt_score}</span></p>
+            <p><b>Descripcion: </b><span id='filmDescription'>${oneDirector.description}</span></p>
+            <p><b>Producer: </b><span id="producer">${oneDirector.producer}</span></p>
+            <p><b>Director: </b><span id="director">${oneDirector.director}</span></p>
+            <p><b>Movie characters:  </b><span id="people">${oneDirector.names}</span></p>
+            </div>
+          </div>  `
+          
+     }
+
+     document.getElementById("informativeFile").innerHTML = directorHTML
+     
+    })
+
+    //SELECCIONAR PRODUCTOR directo DESDE la DATA // ------------------------------Seccion PRODUCTOR
+let selectProd = document.getElementById('selectProducer');
+let producerOptions = data.films.map(function(film) {return film.producer})
+producerOptions = getUniqueValues(producerOptions) //evita duplicacion de elementos
+    console.log(producerOptions);
+    for (let i = 0; i < producerOptions.length; i++){
+        let producerName = producerOptions[i];
+        let prodElement = document.createElement("option")
+        
+        prodElement.textContent = producerName 
+        prodElement.value = producerName
+        selectProd.appendChild(prodElement);
+    }
+
+    selectProd.addEventListener('change', () => {
+     document.getElementById('FirstPage').style.display="none";
+     document.getElementById('informativeFile').style.display = 'block';
+     document.getElementById('character').style.display = 'none';
+     document.getElementById('locations').style.display = 'none';
+     document.getElementById('vehicles').style.display = 'none';
+     document.getElementById('vehiclesFile').style.display = 'none'
+     let dataInput = selectProd.value;
+     let dataFiltered =['']
+     dataFiltered = producerFilter(dataInput,data.films);  
+     const caption = 'He has produced '+ dataFiltered.length + ' films';
+
+     document.getElementById('Name').innerHTML = dataInput;
+     document.getElementById('caption').innerHTML = caption;
+     console.log('Numero de peliculas: '+ dataFiltered.length)
+
+     /* ----- HTML DINAMICO*/
+     let producerHTML = ""
+     dataFiltered.forEach( theProducer=>{
+         theProducer.names = joinPeopleName(theProducer)
+        producerHTML += generatorProducerHTML(theProducer)
+     })
+     function generatorProducerHTML(theProducer){
+         return ` 
+         <div class="flexRow" style="display: flex; font-family: sans-serif; flex-flow: nowrap; 
+         background-color: #D9B8B5; border-radius: 1rem; margin-bottom: 1rem;">
+         <div class="posterFilter" style="padding: 1rem;"><img src="${theProducer.poster}" id="filterImg"></div>
+         <div class="Informative">
+           <h1>${theProducer.title}</h1><hr color="#000">
+           <p><b>Year: </b><span id="year">${theProducer.release_date}</span></p>
+           <p><b>Score: </b><span id="score">${theProducer.rt_score}</span></p>
+           <p><b>Descripcion: </b><span id='filmDescription'>${theProducer.description}</span></p>
+           <p><b>Producer: </b><span id="producer">${theProducer.producer}</span></p>
+           <p><b>Director: </b><span id="director">${theProducer.director}</span></p>
+           <p><b>Movie characters:  </b><span id="people">${theProducer.names}</span></p>
+         </div>
+         </div>`
+
+     }
+
+     document.getElementById("informativeFile").innerHTML = producerHTML
+     
+    })// --- Final producer
+    
+
+ //SELECCIONAR PEOPLE directo DESDE la DATA // ------------------------------Seccion CHARACTER
+let selectCharacter = document.getElementById('selectCharacter');
+let characterOptions = data.films.map(function(film) {return film.title})
+//characterOptions  = getUniqueValues(characterOptions ) //evita duplicacion de elementos
+    console.log(characterOptions );
+
+for(let i=0; i < characterOptions.length; i++){//Porque me lo pone arriba 
+    let titleName = characterOptions[i];
+    let characterElemnt = document.createElement('option')
+
+    characterElemnt.textContent =titleName //¿Por que ?
+    characterElemnt.value = titleName 
+    selectCharacter.appendChild(characterElemnt)
+}
+
+selectCharacter.addEventListener('change',() =>{
+    document.getElementById('FirstPage').style.display = 'none';
+    document.getElementById('informativeFile').style.display = 'none';// Sirve para desaparecer las fichas informativas, si es que estan
+    document.getElementById('character').style.display = 'flex';
+    document.getElementById('locations').style.display = 'none';
+    document.getElementById('vehicles').style.display = 'none';
+    document.getElementById('vehiclesFile').style.display = 'none'
+    
+    let dataInput = selectCharacter.value
+    let dataFiltered = [''];
+    dataFiltered = characterFilter(dataInput, data.films); //argument
+    const caption = 'has '+ dataFiltered.length + ' characters';
+
+    document.getElementById('Name').innerHTML = dataInput;
+    document.getElementById('caption').innerHTML = caption;
+
+    let characterHTML = ""
+    dataFiltered.forEach( theCharacter=>{
+       characterHTML += generatorCharacterHTML(theCharacter)
+    })
+    function generatorCharacterHTML(theCharacter){
+        return `
+        <button class="btnCharacter" value='${theCharacter.img}, ${theCharacter.name}, ${theCharacter.age}, ${theCharacter.specie},${theCharacter.gender},${theCharacter.eye_color},${theCharacter.hair_color}'>
+        <figure class="poster">
+        <img src="${theCharacter.img}" id="characterImg" >
+        <figcaption>${theCharacter.name}</figcaption>
+        </figure></button>`
+
+    }
+    document.getElementById("character").innerHTML =  characterHTML
+    const btnCha = document.getElementsByClassName("btnCharacter")
+
+    for(let i=0; i <btnCha.length; i++){
+        btnCha[i].addEventListener('click', (evt)=>{ // ---- Es un arreglo porque esta en event de class y varios elementos cuentan con esta clase
+            evt.preventDefault;
+            let Character =  btnCha[i].value;
+            let matrizCha = ['']
+            let arrayCha = Character.split(",")
+            matrizCha[0] =arrayCha
+            console.log(matrizCha )
+            
+            let html = ''
+            matrizCha.forEach( theCharacter=>{
+                html += generatorHTMLcharacter(theCharacter)
+             })
+            function generatorHTMLcharacter(theCharacter){
+
+                return` 
+                <img src="${theCharacter[0]}" id="imgCharacter">
+                <div id="infoCharacter"><h1> ${theCharacter[1]} </h1>
+                <p><b>Edad: </b>${theCharacter[2]}</p>
+                <p><b>Especie: </b>${theCharacter[3]}</p>
+                <p><b>Genero: </b>${theCharacter[4]}</p>
+                <p><b>Color de ojos: </b>${theCharacter[5]}</p>
+                <p><b>Color de cabello: </b>${theCharacter[6]}</p></br>
+                </div>`
+
+            }
+            document.getElementById("character").innerHTML = html
+            //hoaaa 
+
+        })
+    }
+   })// final de Character
+///// -----------------------------------------------------------------------seccion Location
+
+let selectLocation = document.getElementById('selectLocation');
+   let locationOptions = titleLocation(data.films)
+   console.log(locationOptions)
+   for(let i=0; i < locationOptions.length; i++){//Porque me lo pone arriba 
+    let Location = document.createElement('option')
+
+
+    Location.textContent =locationOptions[i] //¿Por que ?
+    Location.value = locationOptions[i]
+    selectLocation.appendChild(Location)
+}
+
+selectLocation.addEventListener('change', ()=> {
+    document.getElementById('FirstPage').style.display = 'none';
+    document.getElementById('informativeFile').style.display = 'none';// Sirve para desaparecer las fichas informativas, si es que estan
+    document.getElementById('character').style.display = 'none';
+    document.getElementById('locations').style.display = 'flex';
+    document.getElementById('vehicles').style.display = 'none';
+    document.getElementById('vehiclesFile').style.display = 'none';
+    let dataInput = selectLocation.value
+    let dataFiltered = [''];
+    dataFiltered = locationFilter(dataInput, data.films); //argument
+    console.log(dataFiltered)
+    const caption = 'has'+ dataFiltered.length + ' locations.';
+    document.getElementById('Name').innerHTML = dataInput;
+    document.getElementById('caption').innerHTML = caption;
+
+    /* ----- HTML DINAMICO*/
+    let locationHTML = ""
+    dataFiltered.forEach( thelocation=>{
+        locationHTML += generatorLocationHTML(thelocation)
+    })
+    function generatorLocationHTML(thelocation){
+        return ` 
+        <div class="flexRow" style="display: flex; font-family: sans-serif; 
+        background-color: #BB9EA0; border-radius: 1rem; margin-bottom: 10px; ">
+        <div class="posterFilter" style="padding: 1rem;"><img src="${thelocation.img}" id="filterImg"></div>
+        <div class="informativeLocation">
+          <h1><b></b><span id="title">${thelocation.name}</span></h1>
+          <p><b>Clima: </b><span id="year">${thelocation.climate}</span></p>
+          <p><b>Terreno: </b><span id="score">${thelocation.terrain}</span></p>
+          <p><b>Superficie del agua: </b><span id='filmDescription'>${thelocation.surface_water}</span></p>
+          <p><b>Residentes: </b><span id="producer">${thelocation.residents[0]}</span></p>
+        </div>
+        </div>`
+
+    }
+
+    document.getElementById("locations").innerHTML = locationHTML
+})
+
+
+ let selectVehicle= document.getElementById('selectVehicle');
+ let vehicleOptions = data.films.filter((film) => film.vehicles.length > 0)
+    .map(function(film) {return film.title})
+       console.log(vehicleOptions);
+       for (let i = 0; i < vehicleOptions.length; i++){
+           let vehicleName = vehicleOptions[i];
+           let vehicleElement = document.createElement("option")
+           
+           vehicleElement.textContent = vehicleName 
+           vehicleElement.value = vehicleName
+           selectVehicle.appendChild(vehicleElement);
+       }
+   
+
+
+ selectVehicle.addEventListener('change', () => {
+    document.getElementById('FirstPage').style.display = 'none';
+    document.getElementById('informativeFile').style.display = 'none';// Sirve para desaparecer las fichas informativas, si es que estan
+    document.getElementById('vehicles').style.display = 'flex';
+    document.getElementById('vehiclesFile').style.display = 'flex';
+    document.getElementById('locations').style.display = 'none';
+    let dataInput = selectVehicle.value
+    let dataFiltered = [''];
+    dataFiltered = vehicleFilter(dataInput, data.films); //argument
+    const caption = 'has '+ dataFiltered.length + ' vehicles';
+    console.log(dataFiltered)
+
+    document.getElementById('caption').innerHTML = caption;
+
+    let vehicleHTML = ""
+    dataFiltered.forEach( theVehicle=>{
+       vehicleHTML += generatorVehicleHTML(theVehicle)
+    })
+
+    function generatorVehicleHTML (theVehicle){
+        return `
+        <div class="flexRow" style="display: flex; font-family: sans-serif; flex-flow: nowrap; 
+        background-color: bisque; border-radius: 1rem; margin-bottom: 10px;">
+        <div class="posterFilter" style="padding: 1rem;"><img src="${theVehicle.img}" id="filterImg"></div>
+        <div class="informativeLocation">
+                <p style="font-size: 50px;"><b> ${theVehicle.name}</b></p>
+                <p><b>Description: </b> ${theVehicle.description}<p>
+                <p><b>Pilot: </b>${theVehicle.pilot.name}<p>
+                <p><b>Vehicle Type: </b>${theVehicle.vehicle_class}<p>
+                <p><b>Length </b>${theVehicle.length}</p>
+          </div>
+        </div>
+        `
+    }
+    
+    document.getElementById("vehiclesFile").innerHTML = vehicleHTML
+    document.getElementById("")
+} )
